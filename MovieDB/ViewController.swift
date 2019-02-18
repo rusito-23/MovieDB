f384307da67df66a91e80032cee5e6d44e48a65e
Igor Andruskiewitsch <igor.andruskiewitsch@ar-it11836.local>
2019-02-18 17:11:12 -0300

    Initial Commit

---
+++ MovieDB/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieDB
+//
+//  Created by Igor Andruskiewitsch on 18/02/2019.
+//  Copyright © 2019 Igor Andruskiewitsch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
